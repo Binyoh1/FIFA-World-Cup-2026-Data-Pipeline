de089883351bd802c98e3e4629ba2cb377453774
Binyoh Langhe Theodore <langhetheo@gmail.com>
2026-08-12 14:03:45 +0100

    Update player info processing logic

--- src/transform/transform_players.py
+++ src/transform/transform_players.py
@@ -3,7 +3,7 @@ from prefect import task
 
 from common.paths import players_dir
 
-from transform.utils.data_io import load_data
+from transform.utils.data_helpers import PLAYER_FIELD_MAPPINGS, load_data
 
 
 # load player data
@@ -13,7 +13,7 @@ def load_players() -> pl.LazyFrame:
 
 @task
 def select_player_info(lf: pl.LazyFrame) -> pl.LazyFrame:
-    player_info_lf = (
+    players_lf = (
         lf
         .select(
             # basic player info
@@ -54,13 +54,64 @@ def select_player_info(lf: pl.LazyFrame) -> pl.LazyFrame:
         .unnest('playerInformation')
         .filter(pl.col('title').is_in(["Height", "Preferred foot", "Market value"]))
         .with_columns(
+            # rename additional player info columns
             pl.col('title').str.replace_many(
                 ["Height", "Preferred foot", "Market value"],
-                ["height", "preferred_foot", "transfer_value"]
+                ["height_info", "foot_info", "value_info"]
             ),
         )
-        .drop('translationKey', 'icon', 'countryCode')
-        
+        .drop('translationKey', 'icon', 'countryCode')        
     )
     
-    return player_info_lf
+    # pivot to fold player data back into single row per player
+    players_pivoted_lf = (
+        players_lf
+        .pivot(
+            on='title',
+            on_columns=[
+                'height',
+                'preferred_foot',
+                'transfer_value',
+            ],
+            values='value',
+            index = [
+                'player_id',
+                'player_name',
+                'team_id',
+                'birth_date',
+                'primary_position',
+                'primaryTeam',
+            ],
+            aggregate_function='first'
+        )
+    )
+    
+    # safely parse player info fields, cast to correct dtype, and fallback to null if missing in raw JSON
+    expressions = [
+        pl.col(src).struct.fiel(cfg["field"]).cast(cfg["dtype"]).alias(cfg["alias"])
+        if src in players_pivoted_lf.columns
+        else pl.lit(None).cast(cfg["dtype"]).alias(cfg["alias"])
+        for src, cfg in PLAYER_FIELD_MAPPINGS.items()
+    ]
+    expressions.extend([
+        pl.col('primaryTeam')
+            .struct.field('teamId')
+            .alias('club_id'),
+        pl.col('primaryTeam')
+            .struct.field('teamName')
+            .alias('club'),
+    ])
+    
+    # drop unnecessary columns
+    drop_cols = [src for src in PLAYER_FIELD_MAPPINGS if src in players_pivoted_lf.columns]
+    drop_cols.append('primaryTeam')
+    
+    # consoilidate parsing expressions and fields
+    players_clean_lf = (
+        players_pivoted_lf
+        .with_columns(expressions)
+        .drop(drop_cols)
+    )
+    
+    
+    return players_clean_lf
